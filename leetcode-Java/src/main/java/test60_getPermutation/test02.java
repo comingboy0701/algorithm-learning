21531d3880a0d344e623263e6766405807b72750
comingboy <760855003@qq.com>
2020-05-06 10:16:52 +0800

    update Java

--- leetcode-Java/src/main/java/test60_getPermutation/test02.java
+++
@@ -1,9 +0,0 @@
-package test60_getPermutation;
-
-public class test02 {
-    public static void main(String[] args) {
-        int n = 10, k = 3000;
-        String result = new test01().getPermutation(n, k);
-        System.out.println(result);
-    }
-}
