5292c914ada7ade191d9ef107b20fe17f784f814
comingboy <760855003@qq.com>
2021-03-12 10:01:54 +0800

    leetcode_java-test1052

--- leetcode/src/main/leetcode_java/test1052_maxSatisfied/test.java
+++ leetcode/src/main/leetcode_java/test1052_maxSatisfied/test.java
@@ -2,22 +2,31 @@ package test1052_maxSatisfied;
 
 public class test {
     public static void main(String[] args) {
-
+        int[] customers = {1,2,3,4};
+        int[] grumpy = {1,0,0,0};
+        int res = new test().maxSatisfied(customers, grumpy, 3);
+        System.out.println(res);
     }
 
     public int maxSatisfied(int[] customers, int[] grumpy, int X) {
         int res = 0;
         for (int i = 0; i < customers.length; i++) {
-            if (grumpy[i]!=0){
+            if (grumpy[i]==0){
                 res = res+customers[i];
             }
         }
         int max = 0, cur = 0;
         for (int i = 0, j = 0; i < grumpy.length; i++) {
-            cur += customers[i];
-            if (i - j + 1 > X) cur -= customers[j++];
+            if (grumpy[i]==1){
+                cur = cur+ customers[i];
+            }
+            if (i - j + 1 > X) {
+                if (grumpy[j]==1){
+                    cur = cur-customers[j];
+                }
+                j=j+1;
+            };
             max = Math.max(max, cur);
-
         }
         return max+res;
     }
